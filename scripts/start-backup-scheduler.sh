1570e648f18bfce29310f09adae4f858e385aff1
Takanori Ikeda <takanori.ikeda@toracoya.com>
2026-06-14 23:29:41 +0900

    fix: use crond for backup scheduler

--- scripts/start-backup-scheduler.sh
+++ scripts/start-backup-scheduler.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 set -eu
 
-CRONTAB_FILE=/tmp/spla3-x-log-backup.crontab
+CRONTAB_FILE=/etc/crontabs/root
+ENV_FILE=/tmp/spla3-x-log-backup.env
 BACKUP_SCHEDULE="${BACKUP_SCHEDULE:-0 6 * * *}"
 
-printf '%s /usr/local/bin/backup-postgres-to-r2.sh\n' "$BACKUP_SCHEDULE" > "$CRONTAB_FILE"
+export -p > "$ENV_FILE"
+
+{
+  echo "SHELL=/bin/sh"
+  echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+  printf '%s . %s; /usr/local/bin/backup-postgres-to-r2.sh\n' "$BACKUP_SCHEDULE" "$ENV_FILE"
+} > "$CRONTAB_FILE"
 
 echo "Starting backup scheduler: ${BACKUP_SCHEDULE}"
-exec supercronic "$CRONTAB_FILE"
+exec crond -f -l 8 -L /dev/stdout
